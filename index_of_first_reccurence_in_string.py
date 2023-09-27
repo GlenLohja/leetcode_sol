fb116016584993e5c7ac70f38e8c10d810d2ea76
Glen Lohja <76854498+GlenLohja@users.noreply.github.com>
2023-09-27 13:23:01 +0200

    Create index_of_first_reccurence_in_string.py
    Leetcode easy exercise

---
+++ index_of_first_reccurence_in_string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        occurrence = -1
+        for i in range(len(haystack)):
+            if haystack[i:len(needle) + i] == needle:
+                occurrence = i
+                return occurrence
+            
+        return occurrence
