d3d4dc3f99789b2d5f0c98b35169a1217aba28fb
Glen Lohja <76854498+GlenLohja@users.noreply.github.com>
2023-10-03 22:35:44 +0200

    Create h-index.py

---
+++ array&str/h-index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        citations.sort(reverse=True)
+
+        for i, v in enumerate(citations):
+            if i >= v:
+                return i
+        return len(citations)
