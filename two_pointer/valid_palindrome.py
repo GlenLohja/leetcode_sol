26b5c130ffe3f98d93daa3337c4bff07a3d0ce97
Glen Lohja <76854498+GlenLohja@users.noreply.github.com>
2023-10-06 19:02:55 +0200

    Create valid_palindrome.py

---
+++ two_pointer/valid_palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        cleared = ''.join(ch for ch in s if ch.isalnum()).lower()
+        
+        return cleared == cleared[::-1]
+             
