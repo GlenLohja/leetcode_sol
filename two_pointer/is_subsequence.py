45237ca7ce8f8207d1bfa776feca14f7cc18ed34
Glen Lohja <76854498+GlenLohja@users.noreply.github.com>
2023-10-06 19:02:32 +0200

    Create is_subsequence.py

---
+++ two_pointer/is_subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        if s == "":
+            return True
+
+        l = 0
+
+        for i in range(len(t)):
+            if (l == len(s)):
+                return True
+            if  t[i] == s[l]:
+                l += 1
+
+        return l == len(s)
